43e7bce6b6900227df9d2a71cd5a9ba269204b91
Oliver <oliver@LM4.eu>
2025-09-25 09:30:54 +0200

    feat(build): build infergui and llama-swap frontends

--- scripts/goinfer-pull-run-latest.sh
+++ scripts/goinfer-pull-run-latest.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
 
-command -V go  || { echo REQUIRED: install go  && exit ; }
 command -V git || { echo REQUIRED: install git && exit ; }
+command -V go  || { echo REQUIRED: install go  && exit ; }
+command -V npm || { echo REQUIRED: install npm && exit ; }
 
 set -xe
 
 cd ${0%/*}  # go to the directory of this script
 
-git -C ../../llama-swap switch main
-git -C ../../llama-swap pull
+# --- llama-swap ---
+cd ../..
+[ -d llama-swap ] || git clone https://github.com/mostlygeek/llama-swap
+cd   llama-swap/ui
+git pull
+npm i
+npm run build
 
-git switch swap
+# --- infergui ---
+cd ../..
+[ -d infergui ] || git clone https://github.com/synw/infergui
+cd   infergui
+git pull
+npm i
+npm run build
+
+# --- copy /dist from inferui to goinfer ---
+rm -rf     ../goinfer/go/infer/dist
+cp -r dist ../goinfer/go/infer
+
+# --- goinfer ---
+cd ../goinfer/go
 git pull
 
-# Optional
-# export GOPROXY=https://proxy.golang.org,direct
-# export GODEBUG=toolchaintrace=1
-# go env -w GOTOOLCHAIN=auto
+# --- use best perf ---
+case "$(grep flags -m1 /proc/cpuinfo)" in
+    *" avx512f "*)  export GOAMD64=v4;;
+    *" avx2 "*)     export GOAMD64=v3;;
+    *" sse2 "*)     export GOAMD64=v2;;
+esac
 
-cd ../go
 go run . "@$"
