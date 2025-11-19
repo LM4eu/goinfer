5f40e25ed6722c56286c85c0305c65d24d49e324
Oliver <oliver@LM4.eu>
2025-11-19 21:01:00 +0100

    scripts: deprecate some agent-smith plugins

--- scripts/install-agent-smith-full.sh
+++ scripts/install-agent-smith-full.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+echo "
+to remove all globally installed npm packages:
+
+    npm list -g -p | cut -d/ -f7-8 | xargs npm uninstall -g
+"
+
 set -xe
 
-npm i -g \
+# --prefer-offline = use cache first, else download for internet
+# --prefer-online = download for internet, except 
+npm i -g --prefer-online --no-audit --no-fund \
     @agent-smith/body@latest \
     @agent-smith/brain@latest \
     @agent-smith/cli@latest \
     @agent-smith/feat-git@latest \
     @agent-smith/feat-inference@latest \
     @agent-smith/feat-lang@latest \
-    @agent-smith/feat-models@latest \
-    @agent-smith/feat-search@latest \
-    @agent-smith/feat-vision@latest \
     @agent-smith/jobs@latest \
     @agent-smith/lmtask@latest \
     @agent-smith/smem@latest \
@@ -22,11 +27,20 @@ npm i -g \
     @locallm/browser@latest \
     @locallm/types@latest \
     termollama@latest \
+    \
+    @intrinsicai/gbnfgen@latest \
+    \
+    npm-check-updates@latest \
     ;
 
-# compiler des grammaires gbnf à partir de définitions sour forme d'interfaces typescript
-npm i -g @intrinsicai/gbnfgen@latest
+
+# outdated:
+#    @agent-smith/feat-search@latest
+#    @agent-smith/feat-vision@latest
+#
+# removed:
+#   @agent-smith/feat-models@latest
 
 
-# ncu
-npm i -g npm-check-updates@latest
+# @intrinsicai/gbnfgen = compiler des grammaires gbnf à partir de définitions sour forme d'interfaces typescript
+# npm-check-updates = ncu
