f266b197e9e13fea0a5735d886c3e47c1285b117
Oliver <oliver@LM4.eu>
2025-11-19 21:15:24 +0100

    conf: rename Template -> Params

--- conf/models.go
+++ conf/models.go
@@ -22,14 +22,15 @@ type (
 	// - command-line flags found of file system
 	// - eventual error (if the model is missing or misconfigured).
 	ModelInfo struct {
-		Template *TemplateInfo `json:"template,omitempty,omitzero" yaml:"template,omitempty"`
-		Flags    string        `json:"cmd,omitempty"               yaml:"cmd,omitempty"`
-		Path     string        `json:"path,omitempty"              yaml:"path,omitempty"`
-		Error    string        `json:"error,omitempty"             yaml:"error,omitempty"`
-		Size     int64         `json:"size,omitempty"              yaml:"size,omitempty"`
+		Params *ModelParams `json:"params,omitempty,omitzero" yaml:"params,omitempty"`
+		Flags  string       `json:"cmd,omitempty"             yaml:"cmd,omitempty"`
+		Path   string       `json:"path,omitempty"            yaml:"path,omitempty"`
+		Error  string       `json:"error,omitempty"           yaml:"error,omitempty"`
+		Size   int64        `json:"size,omitempty"            yaml:"size,omitempty"`
 	}
 
-	TemplateInfo struct {
+	// ModelParams provides some model customizations.
+	ModelParams struct {
 		Name  string `json:"name,omitempty"  yaml:"name,omitempty"`
 		Flags string `json:"flags,omitempty" yaml:"flags,omitempty"`
 		Error string `json:"error,omitempty" yaml:"error,omitempty"`
@@ -98,7 +99,7 @@ func (cfg *Cfg) refineModelInfo(name string) {
 // in the directories listed in cfg.ModelsDir (colon-separated).
 // It aggregates matching files, and updates cfg.Info.
 func (cfg *Cfg) updateInfo() {
-	templates := map[string]TemplateInfo{}
+	templates := map[string]ModelParams{}
 	if cfg.Info == nil {
 		cfg.Info = make(map[string]ModelInfo, 16)
 	} else {
@@ -133,7 +134,7 @@ func (cfg *Cfg) updateInfo() {
 	// Put the TemplateInfo in the corresponding ModelInfo
 	for name, ti := range templates {
 		mi := cfg.Info[name]
-		mi.Template = &ti
+		mi.Params = &ti
 		if mi.Flags != "" {
 			mi.Flags = ti.Flags
 			ti.Flags = ""
@@ -144,7 +145,7 @@ func (cfg *Cfg) updateInfo() {
 
 // search walks the given root directory and appends any valid *.gguf model file to
 // cfg.Info. It validates each file using validateFile and warns about errors (logs).
-func (cfg *Cfg) search(templates map[string]TemplateInfo, root string) error {
+func (cfg *Cfg) search(templates map[string]ModelParams, root string) error {
 	return filepath.WalkDir(root, func(path string, dir fs.DirEntry, err error) error {
 		switch {
 		case err != nil:
@@ -167,7 +168,7 @@ func (cfg *Cfg) search(templates map[string]TemplateInfo, root string) error {
 	})
 }
 
-func keepTemplates(templates map[string]TemplateInfo, root, path string) error {
+func keepTemplates(templates map[string]ModelParams, root, path string) error {
 	data, err := os.ReadFile(filepath.Clean(path))
 	if err != nil {
 		return gie.Wrap(err, gie.ConfigErr, "os.ReadFile", "file", path)
@@ -180,7 +181,7 @@ func keepTemplates(templates map[string]TemplateInfo, root, path string) error {
 
 	slog.Debug("Found", "template", path)
 
-	var tpl map[string]TemplateInfo
+	var tpl map[string]ModelParams
 	err = json.Unmarshal(data, &tpl)
 	if err != nil {
 		return gie.Wrap(err, gie.ConfigErr, "json.Unmarshal", "file", path, "100FirsBytes", string(data[:100]))
@@ -211,7 +212,7 @@ func (cfg *Cfg) keepGUFF(root, path string) {
 
 	flags = replaceDIR(path, flags)
 
-	mi := ModelInfo{Template: nil, Flags: flags, Path: path, Error: "", Size: size}
+	mi := ModelInfo{Params: nil, Flags: flags, Path: path, Error: "", Size: size}
 	if old, ok := cfg.Info[name]; ok {
 		slog.Debug("WARN Duplicated models", "dir", root, "name", name, "old", old, "new", mi)
 		mi.Error = "two files have same model name (must be unique)"
