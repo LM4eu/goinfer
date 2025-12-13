8d1acaab3c11d8bf2c3a375abcce07ef2617577a
Oliver <oliver@LM4.eu>
2025-12-13 12:21:48 +0100

    feat(conf): refactor GenLlamaINI() + add test

---
+++ conf/llamacppini_test.go
@@ -0,0 +1,62 @@
+// Copyright 2025 The contributors of Goinfer.
+// This file is part of Goinfer, a LLM proxy under the MIT License.
+// SPDX-License-Identifier: MIT
+
+package conf
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCfg_GenLlamaINI(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		info ModelInfo
+		want string
+	}{
+		{"model-with-flags", ModelInfo{Flags: "  -c   0   --n-gpu-layers 99  --no-jinja 	--context-switch  "}, `
+version = 1
+
+[model-with-flags]
+model = /path/model-with-flags.gguf
+c = 0
+n-gpu-layers = 99
+no-jinja = true
+context-switch = true
+
+[model-with-flags:A]
+model = /path/model-with-flags.gguf
+jinja = true
+chat-template-file = template.jinja
+c = 0
+n-gpu-layers = 99
+no-jinja = true
+context-switch = true
+`},
+		{"model-no-flags", ModelInfo{Flags: ""}, `
+version = 1
+
+[model-no-flags]
+model = /path/model-no-flags.gguf
+
+[model-no-flags:A]
+model = /path/model-no-flags.gguf
+jinja = true
+chat-template-file = template.jinja
+`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			cfg := DefaultCfg()
+			tt.info.Path = "/path/" + tt.name + ".gguf"
+			cfg.Info = map[string]ModelInfo{tt.name: tt.info}
+			got := string(cfg.GenLlamaINI())
+			if strings.TrimSpace(got) != strings.TrimSpace(tt.want) {
+				t.Errorf("GenLlamaINI() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
